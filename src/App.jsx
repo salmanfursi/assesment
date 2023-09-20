69bf99704bcce2de84ee98d69cc269089a640e91
unknown <salmanfursipro@gmail.com>
2023-09-21 04:48:35 +0600

    styled

--- src/App.jsx
+++ src/App.jsx
@@ -3,17 +3,17 @@ import axios from 'axios';
 import "./assets/App.css"
 
 const App = () => {
-  const [beers, setBeers] = useState([]);
+  const [bootles, setBootles] = useState([]);
   const [searchTerm, setSearchTerm] = useState('');
 
   useEffect(() => {
     axios.get('https://api.punkapi.com/v2/beers')
-      .then((response) => setBeers(response.data))
+      .then((response) => setBootles(response.data))
       .catch((error) => console.error(error));
   }, []);
 
-  const filteredBeers = beers.filter((beer) =>
-    beer.name.toLowerCase().includes(searchTerm.toLowerCase())
+  const filteredBootles = bootles.filter((bootle) =>
+    bootle.name.toLowerCase().includes(searchTerm.toLowerCase())
   );
 
   return (
@@ -21,15 +21,15 @@ const App = () => {
       <input
       className='search'
         type="text"
-        placeholder="Search for a beer..."
+        placeholder="Search for a bootle..."
         onChange={(e) => setSearchTerm(e.target.value)}
       />
       <div className="beer-list">
-        {filteredBeers.map((beer) => (
-          <div key={beer.id} className="beer-card">
-            <img src={beer.image_url} alt={beer.name} />
-            <h3>{beer.name}</h3>
-            <p>{beer.tagline}</p>
+        {filteredBootles.map((bootle) => (
+          <div key={bootle.id} className="beer-card">
+            <img src={bootle.image_url} alt={bootle.name} />
+            <h3>{bootle.name}</h3>
+            <p>{bootle.tagline}</p>
           </div>
         ))}
       </div>
